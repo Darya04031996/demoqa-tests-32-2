0ef7603d915afbc03e0885b9a0f70771735acafd
Darya Melgunova <darya.melgunova@gmail.com>
2024-11-24 19:46:57 +0300

    added TextFormTest

---
+++ src/test/java/TextFormTest.java
@@ -0,0 +1,55 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextFormTest {
+    @BeforeAll
+    static void beforeall() {
+        Configuration.browserSize = "920x920";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 5000; // default 4000
+    }
+
+    @Test
+    void fillformTest() {
+        open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue("Darya");
+        $("#lastName").setValue("Melgunova");
+        $("#userEmail").setValue("darya.melgunova@gmail.com");
+        $("#genterWrapper").$(byText("Female")).click();
+        $("#userNumber").setValue("8999543121");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionContainingText("March");
+        $(".react-datepicker__year-select").selectOptionByValue("1996");
+        $(".react-datepicker__day.react-datepicker__day--004").click();
+        $("#subjectsInput").setValue("Computer Science").pressEnter();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/380x240.jfif"));
+        $("#currentAddress").setValue("Street1");
+        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#react-select-4-input").setValue("Delhi").pressEnter();
+        $("#submit").click();
+
+        $(".table").$(byText("Student Name")).sibling(0).shouldHave(text("Darya Melgunova"));
+        $(".table").$(byText("Student Email")).sibling(0).shouldHave(text("darya.melgunova@gmail.com"));
+        $(".table").$(byText("Gender")).sibling(0).shouldHave(text("Female"));
+        $(".table").$(byText("Mobile")).sibling(0).shouldHave(text("8999543121"));
+        $(".table").$(byText("Date of Birth")).sibling(0).shouldHave(text("04 March,1996"));
+        $(".table").$(byText("Subjects")).sibling(0).shouldHave(text("Computer Science"));
+        $(".table").$(byText("Hobbies")).sibling(0).shouldHave(text("Music"));
+        $(".table").shouldHave(text("Picture")).shouldHave(text("380x240.jfif"));
+        $(".table").$(byText("Address")).sibling(0).shouldHave(text("Street1"));
+        $(".table").$(byText("State and City")).sibling(0).shouldHave(text("NCR Delhi"));
+
+    }
+
+}
+
